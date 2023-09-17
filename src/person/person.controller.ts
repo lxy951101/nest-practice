61ada4702071d7f0f7c473655476af9b197b0417
李翔远 <lixiangyuan@up366.com>
2023-09-17 18:58:48 +0800

    feat: 初始化项目

---
+++ src/person/person.controller.ts
@@ -0,0 +1,65 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  UseInterceptors,
+  UploadedFiles,
+} from '@nestjs/common';
+import { PersonService } from './person.service';
+import { CreatePersonDto } from './dto/create-person.dto';
+import { UpdatePersonDto } from './dto/update-person.dto';
+import { AnyFilesInterceptor } from '@nestjs/platform-express';
+
+@Controller('api/person')
+export class PersonController {
+  constructor(private readonly personService: PersonService) {}
+
+  // @Post()
+  // create(@Body() createPersonDto: CreatePersonDto) {
+  //   return this.personService.create(createPersonDto);
+  // }
+
+  @Post('file')
+  @UseInterceptors(
+    AnyFilesInterceptor({
+      dest: 'uploads/',
+    }),
+  )
+  body2(
+    @Body() createPersonDto: CreatePersonDto,
+    @UploadedFiles() files: Array<Express.Multer.File>,
+  ) {
+    console.log(files);
+    return `received: ${JSON.stringify(createPersonDto)}`;
+  }
+
+  @Get()
+  findAll() {
+    return this.personService.findAll();
+  }
+
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.personService.findOne(+id);
+  // }
+
+  @Get('find')
+  query(@Query('name') name: string, @Query('age') age: number) {
+    return `recevied: name=${name},age=${age}`;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updatePersonDto: UpdatePersonDto) {
+    return this.personService.update(+id, updatePersonDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.personService.remove(+id);
+  }
+}
